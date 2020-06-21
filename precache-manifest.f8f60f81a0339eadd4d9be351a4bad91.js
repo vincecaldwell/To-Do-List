03d7f4cf6bb5d11ed58f4b568bc61161e9d8d595
vincent caldwell <vcaldwel8@yahoo.com>
2020-06-21 14:42:25 -0400

    Updates

---
+++ precache-manifest.f8f60f81a0339eadd4d9be351a4bad91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e9e505cb0df6a689603b53dad9c1684",
+    "url": "/To-Do-List/index.html"
+  },
+  {
+    "revision": "adbdaa1615e403edfe1c",
+    "url": "/To-Do-List/static/css/2.36049376.chunk.css"
+  },
+  {
+    "revision": "43bed8082239c41cc03c",
+    "url": "/To-Do-List/static/css/main.4e1a78cb.chunk.css"
+  },
+  {
+    "revision": "adbdaa1615e403edfe1c",
+    "url": "/To-Do-List/static/js/2.f138f123.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-Do-List/static/js/2.f138f123.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43bed8082239c41cc03c",
+    "url": "/To-Do-List/static/js/main.76e61265.chunk.js"
+  },
+  {
+    "revision": "e71b0fca7b414be357cd",
+    "url": "/To-Do-List/static/js/runtime-main.8e2d492d.js"
+  }
+]);
